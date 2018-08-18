63b0b97ad88ad457dc7e81fad56ec4ff0c74d781
Sella Iyer <sella.s.iyer@gmail.com>
2018-08-17 21:02:22 -0500

    config

--- EurekaServiceClient/src/main/java/org/state/service/eureka/client/RestClient.java
+++ EurekaServiceClient/src/main/java/org/state/service/eureka/client/RestClient.java
@@ -1,22 +1,29 @@
 package org.state.service.eureka.client;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
 @Component
+@RefreshScope
 public class RestClient {
 
  @Autowired
  private RestTemplate restTemplate;
- 
-    private static final String URL= "http://eureka-service-demo/nme?name=";
+
+    @Value("${endpoint.url}")
+    private String url = "none";
+
+    @Value("${endpoint.service}")
+    private String serviceEndpoint = "none";
     
     public String getServiceResponse(String name) {
-    	System.out.println(URL + name);
-    	 return restTemplate.exchange(URL + name, HttpMethod.GET, null, String.class).getBody();
+    	System.out.println(url + name);
+    	 return restTemplate.exchange(url+serviceEndpoint + name, HttpMethod.GET, null, String.class).getBody();
     
     	
     	
